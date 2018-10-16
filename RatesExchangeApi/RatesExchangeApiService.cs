2bda657759ae3dc8f9e2970a7500ba085e59cda0
Vassilis Voutsas <voutsas.vasilios@nbg.gr>
2018-10-16 10:39:20 +0300

    parameter 'ToCurrencies' added

--- RatesExchangeApi/RatesExchangeApiService.cs
+++ RatesExchangeApi/RatesExchangeApiService.cs
@@ -16,13 +16,13 @@ namespace RatesExchangeApi
         
         private const string ApiBaseUrl = "https://api.ratesexchange.eu";
         private const string CheckIfApiIsOnLineUrl = "{0}/client/checkapi";
-        private const string GetLatestRatesUrl = "{0}/client/latest?apikey={1}&base_currency={2}";
-        private const string GetLatestDetailsRatesUrl = "{0}/client/latestdetails?apikey={1}&base_currency={2}";
-        private const string GetHistoryRatesUrl = "{0}/client/history?apiKey={1}&base_currency={2}&date={3}";
-        private const string GetHistoryDetailsRatesUrl = "{0}/client/historydetails?apiKey={1}&base_currency={2}&date={3}";
+        private const string GetLatestRatesUrl = "{0}/client/latest?apikey={1}&base_currency={2}&currencies={3}";
+        private const string GetLatestDetailsRatesUrl = "{0}/client/latestdetails?apikey={1}&base_currency={2}&currencies={3}";
+        private const string GetHistoryRatesUrl = "{0}/client/history?apiKey={1}&base_currency={2}&date={3}&currencies={4}";
+        private const string GetHistoryDetailsRatesUrl = "{0}/client/historydetails?apiKey={1}&base_currency={2}&date={3}&currencies={4}";
         private const string GetHistoryRatesForCurrencyUrl = "{0}/client/historydates?apiKey={1}&currency={2}&from_date={3}";
-        private const string ConvertCurrencyUrl = "{0}/client/convert?apiKey={1}&from={2}&amount={3}&date={4}";
-        private const string ConvertCurrencyDetailsUrl = "{0}/client/convertdetails?apiKey={1}&from={2}&amount={3}&date={4}";
+        private const string ConvertCurrencyUrl = "{0}/client/convert?apiKey={1}&from={2}&amount={3}&date={4}&currencies={5}";
+        private const string ConvertCurrencyDetailsUrl = "{0}/client/convertdetails?apiKey={1}&from={2}&amount={3}&date={4}&currencies={5}";
         private const string GetCurrenciesUrl = "{0}/client/currencies?apiKey={1}";
 
         public RatesExchangeApiService(string key)
@@ -37,32 +37,32 @@ namespace RatesExchangeApi
             return resp;
         }
 
-        public async Task<RatesResponse> GetLatestRates(string baseCurrency)
+        public async Task<RatesResponse> GetLatestRates(string baseCurrency, string currencies = null)
         {
             ThrowExceptionIfApiKeyInvalid();
-            var requestUrl = string.Format(CultureInfo.InvariantCulture, GetLatestRatesUrl, ApiBaseUrl, _apiKey, baseCurrency);
+            var requestUrl = string.Format(CultureInfo.InvariantCulture, GetLatestRatesUrl, ApiBaseUrl, _apiKey, baseCurrency, currencies);
             var resp = await HttpHandler.GetResponseFromUrlAsync<RatesResponse>(requestUrl).ConfigureAwait(false);
             return resp;
         }
 
-        public Task<RatesDetailsResponse> GetLatestDetailsRates(string baseCurrency)
+        public Task<RatesDetailsResponse> GetLatestDetailsRates(string baseCurrency, string currencies = null)
         {
             ThrowExceptionIfApiKeyInvalid();
-            var requestUrl = string.Format(CultureInfo.InvariantCulture, GetLatestDetailsRatesUrl, ApiBaseUrl, _apiKey, baseCurrency);
+            var requestUrl = string.Format(CultureInfo.InvariantCulture, GetLatestDetailsRatesUrl, ApiBaseUrl, _apiKey, baseCurrency, currencies);
             return HttpHandler.GetResponseFromUrlAsync<RatesDetailsResponse>(requestUrl);
         }
 
-        public Task<RatesResponse> GetHistoryRates(string baseCurrency, string date)
+        public Task<RatesResponse> GetHistoryRates(string baseCurrency, string date, string currencies = null)
         {
             ThrowExceptionIfApiKeyInvalid();
-            var requestUrl = string.Format(CultureInfo.InvariantCulture, GetHistoryRatesUrl, ApiBaseUrl, _apiKey, baseCurrency, date);
+            var requestUrl = string.Format(CultureInfo.InvariantCulture, GetHistoryRatesUrl, ApiBaseUrl, _apiKey, baseCurrency, date, currencies);
             return HttpHandler.GetResponseFromUrlAsync<RatesResponse>(requestUrl);
         }
 
-        public Task<RatesDetailsResponse> GetHistoryDetailsRates(string baseCurrency, string date)
+        public Task<RatesDetailsResponse> GetHistoryDetailsRates(string baseCurrency, string date, string currencies = null)
         {
             ThrowExceptionIfApiKeyInvalid();
-            var requestUrl = string.Format(CultureInfo.InvariantCulture, GetHistoryDetailsRatesUrl, ApiBaseUrl, _apiKey, baseCurrency, date);
+            var requestUrl = string.Format(CultureInfo.InvariantCulture, GetHistoryDetailsRatesUrl, ApiBaseUrl, _apiKey, baseCurrency, date, currencies);
             return HttpHandler.GetResponseFromUrlAsync<RatesDetailsResponse>(requestUrl);
         }
 
@@ -73,17 +73,17 @@ namespace RatesExchangeApi
             return HttpHandler.GetResponseFromUrlAsync<RatesHistoryResponse>(requestUrl);
         }
 
-        public Task<RatesResponse> ConvertCurrency(string fromCurrency, string amount, string date)
+        public Task<RatesResponse> ConvertCurrency(string fromCurrency, string amount, string date, string currencies = null)
         {
             ThrowExceptionIfApiKeyInvalid();
-            var requestUrl = string.Format(CultureInfo.InvariantCulture, ConvertCurrencyUrl, ApiBaseUrl, _apiKey, fromCurrency, amount, date);
+            var requestUrl = string.Format(CultureInfo.InvariantCulture, ConvertCurrencyUrl, ApiBaseUrl, _apiKey, fromCurrency, amount, date, currencies);
             return HttpHandler.GetResponseFromUrlAsync<RatesResponse>(requestUrl);
         }
 
-        public Task<RatesDetailsResponse> ConvertCurrencyDetails(string fromCurrency, string amount, string date)
+        public Task<RatesDetailsResponse> ConvertCurrencyDetails(string fromCurrency, string amount, string date, string currencies = null)
         {
             ThrowExceptionIfApiKeyInvalid();
-            var requestUrl = string.Format(CultureInfo.InvariantCulture, ConvertCurrencyDetailsUrl, ApiBaseUrl, _apiKey, fromCurrency, amount, date);
+            var requestUrl = string.Format(CultureInfo.InvariantCulture, ConvertCurrencyDetailsUrl, ApiBaseUrl, _apiKey, fromCurrency, amount, date, currencies);
             return HttpHandler.GetResponseFromUrlAsync<RatesDetailsResponse>(requestUrl);
         }
 
